9815670e4f5ab75a84463be48329ff58e73dcf90
Favio Varela <7avio.contact@gmail.com>
2022-08-15 22:28:39 -0500

    Ejercicios tema 3

---
+++ ejercicios_tema-3.java
@@ -0,0 +1,25 @@
+public class Main{    
+    public static void main(String[] args) {
+        // Primera parate - 1st section
+        sum(7, 14, 21);
+        // Segunda parte - Second section
+        Car myCar = new Car();
+        myCar.incrementDoors();
+        System.out.println(myCar.doors);
+    
+    }
+    
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+}
+    class Car {
+        public int doors = 0;
+    
+        public void incrementDoors() {
+            this.doors++;
+        }
+    }
+
+    // By Favio Varela
