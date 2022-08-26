8221e690e6d4ed48ac2ac9ab886ffdd2f0db0ff9
Favio Varela <7avio.contact@gmail.com>
2022-08-26 08:33:57 -0500

    Ejercicio 2, tema 2 added

---
+++ python/tema-2/py_2-1.py
@@ -0,0 +1,6 @@
+#Ejercicio 1
+
+text =  "Este es un texto de prueba para el ejercicio 1 del tema 2"
+
+print(type(text))
+print(text)
---
+++ python/tema-2/py_2-2.py
@@ -0,0 +1,11 @@
+#Ejercicio 1
+
+text =  "Este es un texto de prueba para el ejercicio 1 del tema 2"
+
+print(type(text))
+print(text)
+
+#Ejercicio 2
+
+text =  "Este es el texto definitivo para el ejercicio 2 del tema 2"
+print(text)
