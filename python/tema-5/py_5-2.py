38a2ee3c5668fed0f9a8596936d10b87530c85cd
Favio Varela <7avio.contact@gmail.com>
2022-08-28 20:25:43 -0500

    py_5-2.py main function added

--- python/tema-5/py_5-2.py
+++ python/tema-5/py_5-2.py
@@ -1,6 +1,8 @@
-print('Is your number a prime?')
-number = int(input('Please, type a natural number\n'))
+def check_number():
+    print('Is your number a prime?')
+    number = int(input('Please, type a natural number\n'))
 
-isPrime = (lambda num: 'Yes, {res} is prime'.format(res=num) if (number >= 0 and number // 1 == number and number % 2 != 0 and number != 1) or number == 2 else 'No, {res} isn\'t prime'.format(res=num))(number) 
-print(isPrime)
+    isPrime = (lambda num: 'Yes, {res} is prime'.format(res=num) if (number >= 0 and number // 1 == number and number % 2 != 0 and number != 1) or number == 2 else 'No, {res} isn\'t prime'.format(res=num))(number) 
+    return isPrime
+print(check_number())
 print('by Favio')
