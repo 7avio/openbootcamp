79942472adab67ce4191e593c67c9acb223e06c9
Favio Varela <7avio.contact@gmail.com>
2022-09-01 11:39:27 -0500

    tema-7, sample 2 corrections, added

--- python/tema-7/py_7-2/ejercicio7-2/py_7-2.py
+++ python/tema-7/py_7-2/ejercicio7-2/py_7-2.py
@@ -3,8 +3,8 @@ import time
 localTime = time.localtime()
 
 if localTime.tm_hour >= 19:
-    response = 'Es hora de ir a casa son las {} (horas) {} (minutos) y {} (segundos), terminas a las 19hrs, máquina'.format(localTime.tm_hour, localTime.tm_min, localTime.tm_sec)
+    response = 'It\'s time to go home. It\'s {} hours {} minutes y {} seconds.'.format(localTime.tm_hour, localTime.tm_min, localTime.tm_sec)
     print(response)
 else:
-    response  = 'Quedan {} (horas) {} (minutos) y {} (segundos) para salir'.format(18-localTime.tm_hour, 59-localTime.tm_min, 59-localTime.tm_sec)
+    response  = 'Keep focus. Remain {} hours {} minutes y {} seconds left to go home'.format(18-localTime.tm_hour, 59-localTime.tm_min, 59-localTime.tm_sec)
     print(response)
