c8515996e9277dd49c54fa782351663801a6c3a3
Favio Varela <7avio.contact@gmail.com>
2022-09-02 23:02:57 -0500

    tema-9, concluded

--- python/tema-9/py_9-2/main.py
+++ python/tema-9/py_9-2/main.py
@@ -1,5 +1,19 @@
+from functools import reduce
+
+def getOdds(iterable):
+    return list(filter(lambda x: x % 2 != 0, iterable))
+
+def sumOdds(filtered):
+    return reduce(lambda a, b: a + b, filtered)
+
 def main():
-    pass
+    getList = list(range(1, 101))
+    print(f'The original list is: {getList}')
+    filteredList = getOdds(getList)
+    print(f'The filtered list is: {filteredList}')
+    solution = sumOdds(filteredList)
+    print(f'The cumulative sum is: {solution}')
 
 if __name__ == '__main__':
     main()
+
