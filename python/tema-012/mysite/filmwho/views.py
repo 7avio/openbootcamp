3bd1cb6a3080529ddec8eda797a82a2099567058
Favio Varela <7avio.contact@gmail.com>
2023-01-19 23:17:31 -0600

    Static files integration

--- python/tema-012/mysite/filmwho/views.py
+++ python/tema-012/mysite/filmwho/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 #
 def index(request):
-    item = Director.objects.get(pk=1)
-    context = {'item': item}
-    return render(request, 'filmwho/index.html', context)
+    # item = Director.objects.get(director_id=1)
+    # context = {'item': item}
+    return render(request, 'filmwho/index.html', context=None)
 
--- python/tema-012/mysite/mysite/urls.py
+++ python/tema-012/mysite/mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,4 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('filmwho/', include('filmwho.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
